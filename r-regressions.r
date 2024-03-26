548b16f2739585722f59c57a6f3c463481b13c09
SebSchroeter <sebbl.xd@gmail.com>
2024-03-26 18:08:45 +0100

    started to work on export to r for zoib regression

---
+++ r-regressions.r
@@ -0,0 +1,3 @@
+library(tidyr, help, pos = 2, lib.loc = NULL)
+library(httpgd)
+library(zoib)
---
+++ r_packages.R
@@ -0,0 +1,6 @@
+#master thesis r-code 
+install.packages("languageserver")
+install.packages('feather')
+install.packages('tidyr')
+install.packages('zoib')
+install.packages("httpgd")
